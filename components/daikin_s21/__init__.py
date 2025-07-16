7bbeda103c3f2b6dbcda75967b3e20cbf20f6a5a
bluefoxlee <jerrylee@me.com>
2025-07-16 16:28:08 +0800

    Update __init__.py from original PR #12
    Update __init__.py from original PR #12
    https://github.com/joshbenner/esphome-daikin-s21/pull/12

--- components/daikin_s21/__init__.py
+++ components/daikin_s21/__init__.py
@@ -4,34 +4,49 @@ Daikin S21 Mini-Split ESPHome component config validation & code generation.
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.components import climate, sensor
+from esphome.const import CONF_ID, CONF_SUPPORTED_MODES
+from .. import (
+    daikin_s21_ns,
+    CONF_S21_ID,
+    S21_CLIENT_SCHEMA,
+    DaikinS21Client,
+)
+from esphome.components.climate import ClimateMode
 
-DEPENDENCIES = ["uart"]
+CONF_ROOM_TEMPERATURE_SENSOR = "room_temperature_sensor"
+CONF_SETPOINT_INTERVAL = "setpoint_interval"
 
-CONF_TX_UART = "tx_uart"
-CONF_RX_UART = "rx_uart"
-CONF_S21_ID = "s21_id"
-CONF_DEBUG_PROTOCOL = "debug_protocol"
-
-daikin_s21_ns = cg.esphome_ns.namespace("daikin_s21")
-DaikinS21 = daikin_s21_ns.class_("DaikinS21", cg.PollingComponent)
-DaikinS21Client = daikin_s21_ns.class_("DaikinS21Client")
+DaikinS21Climate = daikin_s21_ns.class_(
+    "DaikinS21Climate", climate.Climate, cg.PollingComponent, DaikinS21Client
+)
 uart_ns = cg.esphome_ns.namespace("uart")
 UARTComponent = uart_ns.class_("UARTComponent")
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(DaikinS21),
-        cv.Required(CONF_TX_UART): cv.use_id(UARTComponent),
-        cv.Required(CONF_RX_UART): cv.use_id(UARTComponent),
-        cv.Optional(CONF_DEBUG_PROTOCOL, default=False): cv.boolean,
-    }
-).extend(cv.polling_component_schema("2s"))
+SUPPORTED_CLIMATE_MODES_OPTIONS = {
+    "OFF": ClimateMode.CLIMATE_MODE_OFF,  # always available
+    "AUTO": ClimateMode.CLIMATE_MODE_AUTO,  # always available
+    "COOL": ClimateMode.CLIMATE_MODE_COOL,
+    "HEAT": ClimateMode.CLIMATE_MODE_HEAT,
+    "DRY": ClimateMode.CLIMATE_MODE_DRY,
+    "FAN_ONLY": ClimateMode.CLIMATE_MODE_FAN_ONLY,
+}
 
-S21_CLIENT_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_S21_ID): cv.use_id(DaikinS21),
-    }
+CONFIG_SCHEMA = cv.All(
+    climate.CLIMATE_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(DaikinS21Climate),
+            cv.Optional(CONF_ROOM_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
+            cv.Optional(
+                CONF_SETPOINT_INTERVAL, default="300s"
+            ): cv.positive_time_period_seconds,
+            cv.Optional(CONF_SUPPORTED_MODES): cv.ensure_list(
+                cv.enum(SUPPORTED_CLIMATE_MODES_OPTIONS, upper=True)
+            ),
+        }
+    )
+    .extend(cv.polling_component_schema("5s"))
+    .extend(S21_CLIENT_SCHEMA)
 )
 
 
@@ -39,7 +54,21 @@ async def to_code(config):
     """Generate code"""
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    tx_uart = await cg.get_variable(config[CONF_TX_UART])
-    rx_uart = await cg.get_variable(config[CONF_RX_UART])
-    cg.add(var.set_uarts(tx_uart, rx_uart))
-    cg.add(var.set_debug_protocol(config[CONF_DEBUG_PROTOCOL]))
+    await climate.register_climate(var, config)
+    s21_var = await cg.get_variable(config[CONF_S21_ID])
+    cg.add(var.set_s21(s21_var))
+    if CONF_ROOM_TEMPERATURE_SENSOR in config:
+        sens = await cg.get_variable(config[CONF_ROOM_TEMPERATURE_SENSOR])
+        cg.add(var.set_room_sensor(sens))
+        if CONF_SETPOINT_INTERVAL in config:
+            cg.add(var.set_setpoint_interval(config[CONF_SETPOINT_INTERVAL]))
+
+    if CONF_SUPPORTED_MODES in config:
+        cg.add(var.set_supported_modes(config[CONF_SUPPORTED_MODES]))
+    else:
+        cg.add(var.set_supported_modes([
+            ClimateMode.CLIMATE_MODE_COOL,
+            ClimateMode.CLIMATE_MODE_HEAT,
+            ClimateMode.CLIMATE_MODE_DRY,
+            ClimateMode.CLIMATE_MODE_FAN_ONLY,
+        ]))
